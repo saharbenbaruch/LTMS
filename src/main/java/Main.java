89e737af840992bec877520ff8b7d30795b8627b
sahar ben baruch <saharbenbaruch@gmail.com>
2020-07-24 13:22:52 +0300

    create parser for new format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,33 @@ import java.util.List;
 public class Main {
 
     public static void main(String [] args){
+
+        // TEST 1 - STRINGS format
         List<String> description= new ArrayList<String>();
-        description.add(" ~ nci | ~ a | nco");
-        description.add( "~ia | nco");
-        description.add( "~ok | a");
-        description.add( "~rf | ia");
-        description.add( "~uf | ia");
-        description.add( "~ok | ~rf");
-        description.add("~ok | ~uf");
-        description.add("~rf | ~uf");
-        description.add( "~a | ~ia");
-//        description.add( "rf");
-//        description.add("ok");
-
         List<String> observation= new ArrayList<String>();
-        observation.add( "rf");
-        observation.add( "ok");
+//        description.add(" ~ nci | ~ a | nco");
+//        description.add( "~ia | nco");
+//        description.add( "~ok | a");
+//        description.add( "~rf | ia");
+//        description.add( "~uf | ia");
+//        description.add( "~ok | ~rf");
+//        description.add("~ok | ~uf");
+//        description.add("~rf | ~uf");
+//        description.add( "~a | ~ia");
+//
+
+//        observation.add( "rf");
+//        observation.add( "ok");
+     //   ProblemSolver solver= new ProblemSolver(Format.STRINGS,description, observation);
+       // ArrayList<Clause> conflicts =solver.solve();
+        ///**********************************************
+
+        //TEST 2 - elctrical ciruits
 
 
-        ProblemSolver solver= new ProblemSolver(description, observation);
+
+
+        ProblemSolver solver= new ProblemSolver(Format.ELECTRICALCIRCUITS,description, observation);
         ArrayList<Clause> conflicts =solver.solve();
 
 
--- src/main/java/ProblemSolver.java
+++ src/main/java/ProblemSolver.java
@@ -1,15 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+enum Format { STRINGS, ELECTRICALCIRCUITS }
 public class ProblemSolver {
     private TrueMaintenanceSystem ltms=new TrueMaintenanceSystem();
     private List<String> description;
     private List<String> observation;
     private List <Clause> clauses=new ArrayList<Clause>();
+    private Format format;
 
-    public ProblemSolver(List<String> description, List<String> observation) {
+    public Format getFormat() {
+        return format;
+    }
+
+    public ProblemSolver(Format problemFormat, List<String> description, List<String> observation) {
         this.description = description;
         this.observation= observation;
+        this.format = problemFormat;
     }
 
     /**
@@ -25,17 +35,66 @@ public class ProblemSolver {
      * parse description and observation from String (our input) -> clauses
      */
     private void parseClauses() {
-        for (String desc :description){
-            clauses.add(parse(desc));
+
+        if(getFormat()== Format.ELECTRICALCIRCUITS){
+            /**
+             *      get paths of description and observations
+              */
+
+            Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter path for circuits description: ");
+
+            String circuitDescriptionPath = scanner.nextLine();  // Read user input
+            System.out.println("Path for circuit description is: " + circuitDescriptionPath);  // Output user input
+
+            System.out.println("Enter path for observations: ");
+            String observationsPath= scanner.nextLine();
+            System.out.println("Path for observations is: " + observationsPath);  // Output user input
+
+            String descContent= readFile(circuitDescriptionPath);
+            String obsContent= readFile(observationsPath);
+
+            CircuitParserFormat parser =new CircuitParserFormat();
+            description =parser.parseElectricalCircuitDescription(descContent);
+            observation= parser.parseElectricalCircuitObservation(obsContent);
+
         }
-        for (String obs :observation){
-            clauses.add(parse(obs));
+
+        for (String desc : description) {
+            clauses.add(parseStringsFormat(desc));
+        }
+        for (String obs : observation) {
+            clauses.add(parseStringsFormat(obs));
         }
     }
+
     /**
-     * parse strings to clauses
+     *
+     * @param path path of file
+     * @return content of file as string
      */
-    private Clause parse(String desc) {
+    private String readFile(String path){
+        String content="";
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                content+=data;
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return content;
+    }
+    /**
+     * parse strings to clause
+     */
+    private Clause parseStringsFormat(String desc) {
         //remove spaces.
         List<Literal> literals=new ArrayList<Literal>();
         desc=desc.replaceAll("\\s+","");
