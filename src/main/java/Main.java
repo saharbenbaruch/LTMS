4d08e56ca5bd4e6ceae344a28a69381e9bff9c03
sahar ben baruch <saharbenbaruch@gmail.com>
2020-07-30 09:21:38 +0300

    Revert "fix example"
    This reverts commit 8e5a3618

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,33 +4,23 @@ import java.util.List;
 public class Main {
 
     public static void main(String [] args){
-
-        // TEST 1 - STRINGS format
         List<String> description= new ArrayList<String>();
+        description.add(" ~ nci | ~ a | nco");
+        description.add( "~ia | nco");
+        description.add( "~ok | a");
+        description.add( "~rf | ia");
+        description.add( "~uf | ia");
+        description.add( "~ok | ~rf");
+        description.add("~ok | ~uf");
+        description.add("~rf | ~uf");
+        description.add( "~a | ~ia");
+
         List<String> observation= new ArrayList<String>();
-//        description.add(" ~ nci | ~ a | nco");
-//        description.add( "~ia | nco");
-//        description.add( "~ok | a");
-//        description.add( "~rf | ia");
-//        description.add( "~uf | ia");
-//        description.add( "~ok | ~rf");
-//        description.add("~ok | ~uf");
-//        description.add("~rf | ~uf");
-//        description.add( "~a | ~ia");
-//
-
-//        observation.add( "rf");
-//        observation.add( "ok");
-     //   ProblemSolver solver= new ProblemSolver(Format.STRINGS,description, observation);
-       // ArrayList<Clause> conflicts =solver.solve();
-        ///**********************************************
-
-        //TEST 2 - elctrical ciruits
+        observation.add( "rf");
+        observation.add( "ok");
 
 
-
-
-        ProblemSolver solver= new ProblemSolver(Format.ELECTRICALCIRCUITS,description, observation);
+        ProblemSolver solver= new ProblemSolver(description, observation);
         ArrayList<Clause> conflicts =solver.solve();
 
 
